020512c46bcdca07263cad5d0501127d623cdc01
dimitris <dimgatz98@gmail.com>
2022-11-23 13:34:16 +0200

    feat: adding gRPC server

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	labeler "github.com/dimgatz98/labeler"
-
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-)
+import labeler "github.com/dimgatz98/labeler/src"
 
 func main() {
-	config, err := clientcmd.BuildConfigFromFlags("", "/home/dimitris/.kube/config")
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-
-	_, err = labeler.ListNodes(clientset)
-	if err != nil {
-		panic(err)
-	}
-
-	params := labeler.PatchNodeParam{
-		Node:         "k3d-k3s-default-server-0",
-		OperatorType: "replace",
-		OperatorPath: "/metadata/labels/",
-		OperatorData: map[string]interface{}{
-			"all-2g.12gb": "true",
-		},
-	}
-	res, err := labeler.PatchNode(clientset, params)
-	if err != nil {
-		panic(err)
-
-	}
-	fmt.Println(res)
+	labeler.Serve()
 }
---
+++ src/server.go
@@ -0,0 +1,27 @@
+package labeler
+
+import (
+	"log"
+	"net"
+
+	labeler "github.com/dimgatz98/labeler/src/server"
+
+	"google.golang.org/grpc"
+)
+
+func Serve() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	s := Server{}
+
+	grpcServer := grpc.NewServer()
+
+	labeler.RegisterLabelerServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+}
